77611ae5d2c20bcbd9c9d52cc3204fed915b0b50
Mike Warren <mikeywarren89@gmail.com>
2020-02-19 15:19:15 +0900

    styled pages, added views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,16 @@
-10.times do
-  Restaurant.create!(
-    name: Faker::Restaurant.name,
-    address: Faker::Address.street_address,
-    phone_number: Faker::PhoneNumber.cell_phone,
-    category: Restaurant::CATEGORIES.sample
-    )
+# 10.times do
+#   Restaurant.create!(
+#     name: Faker::Restaurant.name,
+#     address: Faker::Address.street_address,
+#     phone_number: Faker::PhoneNumber.cell_phone,
+#     category: Restaurant::CATEGORIES.sample
+#     )
+# end
+
+100.times do
+  Review.create!(
+    content: Faker::Restaurant.review,
+    rating: rand(5),
+    restaurant: Restaurant.find(rand(10))
+  )
 end
