ae87132e5c27a87b9650cfd3613182b037548960
Mike Warren <mikeywarren89@gmail.com>
2020-02-19 16:01:28 +0900

    converted to postgres

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-# 10.times do
-#   Restaurant.create!(
-#     name: Faker::Restaurant.name,
-#     address: Faker::Address.street_address,
-#     phone_number: Faker::PhoneNumber.cell_phone,
-#     category: Restaurant::CATEGORIES.sample
-#     )
-# end
+10.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: Restaurant::CATEGORIES.sample
+    )
+end
 
 100.times do
   Review.create!(
